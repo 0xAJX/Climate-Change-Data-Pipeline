c7ddf3b6cbb34b30ad01cfae5d08f722ee424139
Abhishek Jadhav <abhishek.jadhav64@gmail.com>
2022-05-15 23:24:38 +0530

    first commit

---
+++ streamlit_app.py
@@ -0,0 +1,23 @@
+import re
+from os import listdir
+from os.path import isfile, join
+import numpy as np
+import streamlit as st
+import pandas as pd
+import datetime
+
+st.write("Average Temperature Based On Year For India")
+working_directory = "ClimateChangeModified"
+files = [f for f in listdir(working_directory) if isfile(join(working_directory, f))]
+
+df = None
+
+for f in files:
+    if re.search(".csv$", f):
+        df = pd.read_csv(working_directory + "/" + f, names=["date", "death"])
+
+if df is None:
+    raise Exception("Issue with reading file")
+
+df.set_index('year', inplace=True)
+st.area_chart(df)
