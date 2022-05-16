ffa79b1521b4198459d4828e7842df0866326b6a
Abhishek Jadhav <abhishek.jadhav64@gmail.com>
2022-05-16 14:07:00 +0530

    Streamlit Changes

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,17 +12,19 @@ df = None
 
 for f in files:
     if re.search(".csv$", f):
-        df = pd.read_csv(working_directory + "/" + f, names=["year", "averagetemperature"])
+        df = pd.read_csv(working_directory + "/" + f, names=["Year", "Average Temperature"])
+
+udf = df
 
 if df is None:
     raise Exception("Issue with reading file")
 
-df.set_index('year', inplace=True)
+df.set_index('Year', inplace=True)
 
-col1, col2 = st.columns([3, 1])
+col1, col2 = st.columns([4, 2])
 
 col1.subheader("Average Temperature Based On Year For India")
 st.line_chart(df)
 
 col2.subheader("Sneak Peek to Data")
-col2.write(df)
+col2.write(udf)
