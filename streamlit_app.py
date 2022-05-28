c6caf13d466b7102764a1725ec62bf20ed79a649
Abhishek <abhishek.jadhav64@gmail.com>
2022-05-28 18:25:39 +0530

    Testing

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ from os import listdir
 from os.path import isfile, join
 import streamlit as st
 import pandas as pd
+import altair as alt
 
 st.header("Climate Change")
 working_directory = "ClimateChangeModified"
@@ -29,7 +30,7 @@ mumbai_df = mumbai_df[["Date", "temp"]]
 
 india_df.set_index('Year', inplace=True)
 # cities_df.set_index(['Date', "Average Temperature"], inplace=True)
-mumbai_df.set_index('Date', inplace=True)
+# mumbai_df.set_index('Date', inplace=True)
 
 container1 = st.container()
 container1.write("Average Temperature Based On Year For India")
@@ -39,7 +40,9 @@ container2 = st.container()
 container2.write("Sneak Peek to Data")
 container2.write(india_df)
 
-container3 = st.container()
-container3.write("Recent Temperature in Mumbai")
-container3.line_chart(mumbai_df)
+# container3 = st.container()
+# container3.write("Recent Temperature in Mumbai")
+# container3.line_chart(mumbai_df)
 #%%
+c = alt.Chart(mumbai_df).mark_circle().encode(
+    x='Date', y='temp', size='c', color='c')
